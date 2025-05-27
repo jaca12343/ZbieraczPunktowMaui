69e819d7b06bb654293b05470bc4bd93ea5873ef
jdombrowski1 <jdombrowski1@S3002>
2025-05-27 14:12:55 +0200

    k

--- ZbieraczPunktowMaui/MainPage.xaml.cs
+++ ZbieraczPunktowMaui/MainPage.xaml.cs
@@ -1,16 +1,33 @@
-﻿namespace ZbieraczPunktowMaui
+﻿using Microsoft.Maui.Platform;
+using Windows.UI.Xaml;
+
+namespace ZbieraczPunktowMaui
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
         Player player;
+        Player player2;
         public MainPage()
         {
             InitializeComponent();
             player = new Player();
-            mainLayout.Children.Add(player.image);
+            player.addToBoard(mainLayout, 50, 50);
+            player2 = new Player();
+            player2.addToBoard(mainLayout, 200, 50);
+            var handler = mainLayout.Handler;
+            UIElement? nativeView = handler?.PlatformView as UIElement;
+            if (nativeView != null)
+            {
+                nativeView.KeyDown += this.PlatformView_KeyDown;
+                nativeView.KeyUp += this.PlatformView_KeyUp;
+                nativeView.PreviewKeyDown += this.PlatformView_PreviewKeyDown;
+            }
         }
-        
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
     }
 
 }
--- ZbieraczPunktowMaui/Player.cs
+++ ZbieraczPunktowMaui/Player.cs
@@ -12,13 +12,19 @@ namespace ZbieraczPunktowMaui
         int width;
         int[] Positon;
         public Image image;
+        
         public Player() { 
             height = 100; 
             width = 100; 
             Positon = new int[2];
             image = new Image();
             image.Source = ImageSource.FromFile("player.png");
-            
+        }
+        public void addToBoard(dynamic Board, int x, int y)
+        {
+            Board.Children.Add(image);
+            Positon[0] = x; Positon[1] = y;
+            AbsoluteLayout.SetLayoutBounds(image, new Rect(x, y, 50, 50));
         }
     }
 }
