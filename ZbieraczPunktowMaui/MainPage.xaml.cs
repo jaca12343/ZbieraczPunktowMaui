30e80d3a77a23e3da57986dc5a973e247a27c0a8
uczen <uczen@S6004>
2025-05-27 11:11:29 +0200

    player

--- ZbieraczPunktowMaui/MainPage.xaml.cs
+++ ZbieraczPunktowMaui/MainPage.xaml.cs
@@ -3,23 +3,14 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
+        Player player;
         public MainPage()
         {
             InitializeComponent();
+            player = new Player();
+            mainLayout.Children.Add(player.image);
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        
     }
 
 }
---
+++ ZbieraczPunktowMaui/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZbieraczPunktowMaui
+{
+    internal class Player
+    {
+        int height;
+        int width;
+        int[] Positon;
+        public Image image;
+        public Player() { 
+            height = 100; 
+            width = 100; 
+            Positon = new int[2];
+            image = new Image();
+            image.Source = ImageSource.FromFile("player.png");
+            
+        }
+    }
+}
