87609062d2ef2a8e0f170dc0e2dcea9536e40631
uczen <uczen@S6004>
2025-05-27 10:56:51 +0200

    Dodaj pliki projektów.

---
+++ ZbieraczPunktowMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ZbieraczPunktowMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ZbieraczPunktowMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ZbieraczPunktowMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
